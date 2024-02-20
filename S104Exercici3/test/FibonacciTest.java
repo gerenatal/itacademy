a8312aa32b6d3a40738e54af85d18da4c0aebe1c
Geremias Bristot <42945299+gerenatal@users.noreply.github.com>
2024-02-20 11:40:30 +0100

    Add files via upload

---
+++ S104Exercici3/test/FibonacciTest.java
@@ -0,0 +1,23 @@
+import com.mycompany.s104exercici3.Fibonacci;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FibonacciTest {
+    
+    public FibonacciTest() {
+    }
+    
+    @Test
+    void testSinCantidad(){
+        assertThrows(IllegalArgumentException.class,
+        () -> new Fibonacci(-1));
+    }
+
+    @Test
+    void testArrayIndexOutOfBoundsException() {
+        Fibonacci fibonacci = new Fibonacci(5);
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            int numero = fibonacci.getLista()[5]; // Trying to access index 5, which is out of bounds
+        });
+    }
+}
