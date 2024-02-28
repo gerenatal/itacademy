ef93256999c3d0dc4fc9adbc57a89fb7f97913b2
Geremias Bristot <42945299+gerenatal@users.noreply.github.com>
2024-02-28 13:11:12 +0100

    Update S102exercici1.java

--- S102exercici2/S102exercici1.java
+++ S102exercici2/S102exercici1.java
@@ -8,7 +8,7 @@ public class S102exercici1 {
         
         Venda client1 = new Venda();
         
-        /* Crear y anadir produtos
+        /*
         Producte azucar = new Producte("azucar",2);
         Producte chocolata = new Producte("chocolata", 2.5);
         Producte queso = new Producte("queso", 3.4);
@@ -19,13 +19,10 @@ public class S102exercici1 {
         */
         
         try{
-            //Si la cesta esta vacia:
             client1.calcularTotal();
             
-            //Testear el indexOutOfBoundsException
             System.out.println(client1.getCesta().get(4).toString());
-            
-            //No se puede testear los dos exceptions juntos, se testa uno y después el otro 
+             
         }catch(VendaBuidaException e){
             System.out.println(e.getMessage());
         }catch(ArrayIndexOutOfBoundsException e){
