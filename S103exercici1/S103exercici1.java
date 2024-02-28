03c8f32de26ca2c4f9ad551affd10c2ff459d047
Geremias Bristot <42945299+gerenatal@users.noreply.github.com>
2024-02-28 13:09:58 +0100

    Add files via upload

--- S103exercici1/S103exercici1.java
+++ S103exercici1/S103exercici1.java
@@ -13,30 +13,22 @@ public class S103exercici1 {
             "july", "september", "october", "november", "december"};
         ArrayList <Month> year = new ArrayList<>();
         
-        //adding the months with its corresponding names in the arraylist
         for(int i = 0; i < 11; i++){
             year.add(new Month(months[i]));
         }
         
-        //printing to check if it did it right
         for(int i = 0; i < year.size(); i++){
             System.out.print(year.get(i).getName() + ", ");
         }
-        //space, it´s important
         System.out.println();
         
-        //adding the august month
         year.add(7, new Month("august"));
         
-        //checking its order
         for(int i = 0; i < year.size(); i++){
             System.out.print(year.get(i).getName() + ", ");
         }
-        
-        //space, the final frontier
         System.out.println();
         
-        //traversing through the list with iterator
         Iterator<Month> iterator = year.iterator();
         while(iterator.hasNext()){
             Month month = iterator.next();
@@ -44,19 +36,15 @@ public class S103exercici1 {
         }
         System.out.println();
         
-        //adding one more duplicate month to check the HashSet later
         year.add(year.get(6));
         
-        //printing
         for(int i = 0; i < year.size(); i++){
             System.out.print(year.get(i).getName() + ", ");
         }
         System.out.println();
         
-        //converting the ArrayList into a HashSet
         HashSet<Month> yearHash = new HashSet<>(year);
         
-        //printing
         Iterator<Month> iterator2 = yearHash.iterator();
         while(iterator2.hasNext()){
             Month month = iterator2.next();
